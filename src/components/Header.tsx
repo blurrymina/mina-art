a34faa8f7f6df01900232552584ddcb229c255e3
abdo <abdo@ABDOALAA>
2025-02-03 23:10:45 +0200

    Your commit message here

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,19 +8,6 @@ const Header: React.FC = () => {
     i18n.changeLanguage(lang);
   };
 
-  const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, sectionId: string) => {
-    e.preventDefault(); // Prevent default anchor link behavior
-
-    // Scroll to the section
-    const target = document.getElementById(sectionId);
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' });
-      
-      // Clear the URL without reloading the page
-      window.history.pushState({}, '', window.location.pathname);
-    }
-  };
-
   return (
     <header>
       <div className="logo" data-key="logo">
@@ -36,10 +23,10 @@ const Header: React.FC = () => {
       </div>
       <nav>
         <ul>
-          <li><a href="#hero" data-key="home" onClick={(e) => handleClick(e, 'hero')}>{t('home')}</a></li>
-          <li><a href="#gallery" data-key="gallery" onClick={(e) => handleClick(e, 'gallery')}>{t('gallery')}</a></li>
-          <li><a href="#painting-game" data-key="draw" onClick={(e) => handleClick(e, 'painting-game')}>{t('draw')}</a></li>
-          <li><a href="#about" data-key="about" onClick={(e) => handleClick(e, 'about')}>{t('about')}</a></li>
+          <li><a href="#hero" data-key="home">{t('home')}</a></li>
+          <li><a href="#gallery" data-key="gallery">{t('gallery')}</a></li>
+          <li><a href="#painting-game" data-key="draw">{t('draw')}</a></li>
+          <li><a href="#about" data-key="about">{t('about')}</a></li>
         </ul>
       </nav>
     </header>
